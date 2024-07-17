8ee30a15515da2becddad2d0b23ef03416cc93f3
Developer <>
2024-07-17 14:49:01 +0300

    Rollback obsolete extension methods.

--- Sources/Extension/CMSampleBuffer+Extension.swift
+++ Sources/Extension/CMSampleBuffer+Extension.swift
@@ -17,4 +17,18 @@ extension CMSampleBuffer {
             sampleAttachments[0][.notSync] = newValue ? 1 : nil
         }
     }
+    
+    @available(iOS, obsoleted: 13.0)
+    @available(tvOS, obsoleted: 13.0)
+    @available(macOS, obsoleted: 10.15)
+    var dataBuffer: CMBlockBuffer? {
+        get {
+            CMSampleBufferGetDataBuffer(self)
+        }
+        set {
+            _ = newValue.map {
+                CMSampleBufferSetDataBuffer(self, newValue: $0)
+            }
+        }
+    }
 }
