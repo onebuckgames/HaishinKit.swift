6254f766609292f0f92f22041a1db60f8dd4cae8
Developer <developer@onebuckgames.com>
2025-01-16 23:01:11 +0200

    Enable PIP for iOS.

--- HaishinKit/Sources/Mixer/VideoCaptureUnit.swift
+++ HaishinKit/Sources/Mixer/VideoCaptureUnit.swift
@@ -134,10 +134,6 @@ final class VideoCaptureUnit: CaptureUnit {
 
     @available(tvOS 17.0, *)
     func setBackgroundMode(_ background: Bool) {
-        guard !session.isMultitaskingCameraAccessEnabled else {
-            return
-        }
-        
         if #available(iOS 16.0, *) {
             guard !session.isMultitaskingCameraAccessEnabled else {
                 return
