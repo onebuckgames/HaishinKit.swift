a8e9ffb7bd7c401ce2c8ecd0a1269f668974c66e
Developer <developer@onebuckgames.com>
2026-04-30 11:07:15 +0300

    Fixed compilation error.

--- Sources/IO/IOStream.swift
+++ Sources/IO/IOStream.swift
@@ -613,7 +613,6 @@ extension IOStream: IOScreenCaptureUnitDelegate {
 
 #if os(macOS)
 extension IOStream: SCStreamOutput {
-    @available(macOS 12.3, *)
     public func stream(_ stream: SCStream, didOutputSampleBuffer sampleBuffer: CMSampleBuffer, of type: SCStreamOutputType) {
         if #available(macOS 13.0, *) {
             switch type {
